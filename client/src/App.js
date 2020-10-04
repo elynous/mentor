504f3af97537cc1a59821d59326c9d9b07298ac8
Jonathan Palacio <jpalacio0612@gmail.com>
2020-10-04 17:51:04 -0500

    React-Router-Dom Added

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,19 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.css";
 import { Header } from "./components/Header/Header";
 import { Hero } from "./components/Hero/Hero";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Hero />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Hero} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
